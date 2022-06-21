9485e364bb701e6856637ceeb7da65d0ae2eae27
rohithvijayan <rohithvij20@gmail.com>
2022-06-22 00:24:54 +0530

    added some changes

--- Soccer_Bot.py
+++ Soccer_Bot.py
@@ -5,10 +5,12 @@ from telebot import TeleBot
 bot=TeleBot("5564377267:AAEUWTaHteOE6-zhIvBA2w6yYfe5iDrgi-g")
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id,'HEY')
+    bot.send_message(message.chat.id,'Heyy SoccerDaily here!\nGet latest updates from major leagues and tournaments,\nTransfer Updates and Rumours an more:)')
+    bot.send_animation(message.chat.id,animation="https://tenor.com/bFRzH.gif")
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.send_message(message.chat.id,'YES PLSS')
+    bot.send_message(message.chat.id,'Sorry with the issue,will try to resolve soon!')
+    bot.send_animation(message.chat.id,animation="https://tenor.com/bCB80.gif")
 client=newsapi.NewsApiClient("6e7e06236b6f422ebb6a0fa21532e4c5")
 topnews=client.get_top_headlines(sources='four-four-two',language='en',page_size=5,page=1)
 articles=topnews['articles']
