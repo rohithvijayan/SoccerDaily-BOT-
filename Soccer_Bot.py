099fa640813851e9b7aff72632afc6b74d97a1c6
rohithvijayan <rohithvij20@gmail.com>
2022-06-22 19:30:29 +0530

    api key exposed-fixed

--- Soccer_Bot.py
+++ Soccer_Bot.py
@@ -2,7 +2,7 @@ import requests
 import newsapi
 from telebot import TeleBot
 
-bot=TeleBot("5564377267:AAEUWTaHteOE6-zhIvBA2w6yYfe5iDrgi-g")
+bot=TeleBot("")
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id,'Heyy SoccerDaily here!\nGet latest updates from major leagues and tournaments,\nTransfer Updates and Rumours an more:)')
@@ -11,7 +11,7 @@ def start(message):
 def help(message):
     bot.send_message(message.chat.id,'Sorry with the issue,will try to resolve soon!')
     bot.send_animation(message.chat.id,animation="https://tenor.com/bCB80.gif")
-client=newsapi.NewsApiClient("6e7e06236b6f422ebb6a0fa21532e4c5")
+client=newsapi.NewsApiClient("")
 topnews=client.get_top_headlines(sources='four-four-two',language='en',page_size=5,page=1)
 articles=topnews['articles']
 @bot.message_handler(commands=['news'])
